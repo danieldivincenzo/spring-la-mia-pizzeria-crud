c264d47cd3ed110c71f7e178e28d85bd72086289
danieldivincenzo <danieldivincenzo14@gmail.com>
2025-09-22 17:55:57 +0200

    inserito dettaglio

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,11 +1,13 @@
 package com.example.demo.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.model.Pizza;
@@ -13,8 +15,9 @@ import com.example.demo.repository.PizzaRepository;
 
 
 
+
 @Controller
-@RequestMapping("/home")
+@RequestMapping("/pizze")
 public class MainController {
 
     @Autowired
@@ -24,7 +27,19 @@ public class MainController {
     public String index(Model model) {
         List <Pizza> risultato = repository.findAll();
         model.addAttribute("lista", risultato);
-        return "index";
+        return "/pizze/index";
+    }
+    
+    @GetMapping("/show/{id}")
+    public String show(@PathVariable("id") Integer id, Model model) {
+        Optional <Pizza> optionalPizza = repository.findById(id);
+        if(optionalPizza.isPresent()){
+            model.addAttribute("pizza", optionalPizza.get());
+            model.addAttribute("empty", false);
+        } else {
+            model.addAttribute("empty", true);
+        }
+        return "/pizze/show";
     }
     
 }
